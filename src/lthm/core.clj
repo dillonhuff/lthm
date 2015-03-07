5571a12e5738e4032b762e3d7940900f644b384d
Dillon Huff <dillonhuff@gmail.com>
2015-03-07 03:16:44 -0600

    term creation and type checking functions pass tests

--- src/lthm/core.clj
+++ src/lthm/core.clj
@@ -1,12 +1,22 @@
 (ns lthm.core
   (:gen-class))
 
-(defstruct variable :name)
+(defstruct variable :type :name)
 
-(defstruct function :name :args)
+(defstruct function :type :name :args)
+
+(defn- datatype [core-struct]
+  (:type core-struct))
 
 (defn v [name]
-  (struct variable name))
+  (struct variable 'variable name))
 
 (defn func [name args]
-  (struct function name args))
+  (struct function 'function name args))
+
+;; Type testing
+(defn v? [obj]
+  (= (datatype obj) 'variable))
+
+(defn func? [obj]
+  (= (datatype obj) 'function))
--- test/lthm/core_test.clj
+++ test/lthm/core_test.clj
@@ -10,4 +10,15 @@
   (testing "function creation"
     (is (= (func "f" []) (func "f" [])))
     (is (not (= (func "f" [v "a"]) (func "f" [v "b"]))))
-    (is (not (= (func "g" [v "b"]) (func "k" [v "b"]))))))
+    (is (not (= (func "g" [v "b"]) (func "k" [v "b"]))))
+    (is (=
+         (func "loves" [v "John", v "Kate"])
+         (func "loves" [v "John", v "Kate"]))))
+
+  (testing "variable type"
+    (is (= (v? (v "a")) true))
+    (is (= (v? (func "f" [])) false)))
+
+  (testing "function type"
+    (is (= (func? (func "f" [v "b"])) true))
+    (is (= (func? [v "asdf"]) false))))
